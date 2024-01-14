57e321139833cc073494b13da90a8948a06df1c9
Mir <mir@gmail.com>
2024-01-14 19:39:42 +0500

    Feature Divide Implemeneted

--- Calculator.cpp
+++ Calculator.cpp
@@ -7,7 +7,15 @@ using namespace std;
 double add(double num1, double num2);
 double subtract(double num1, double num2);
 double multiply(double num1, double num2);
-double divide(double num1, double num2);
+double divide(double num1, double num2) {
+    if (num2 == 0) {
+        cout << "Error: Division by zero" << endl;
+        return NAN; // Handle division by zero
+    } else {
+        return num1 / num2;
+    }
+}
+
 
 // Function declarations for advanced operations (Momina)
 double power(double base, double exponent);
