9ab95ab616eecfa0722b00ce5481f4e23117542e
Mir <mir@gmail.com>
2024-01-14 19:33:19 +0500

    Feature Add Implemented

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,7 +4,9 @@
 using namespace std;
 
 // Function declarations for core calculator operations (Mir Fahad)
-double add(double num1, double num2);
+double add(double num1, double num2) {
+    return num1 + num2;
+}
 double subtract(double num1, double num2);
 double multiply(double num1, double num2);
 double divide(double num1, double num2);
